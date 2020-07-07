c3b20f6d6cc2c495403b0d5a2a86e6632dc41def
Stoyan Dimitrov <stoyan.dimitrov@dtms.de>
2020-07-07 11:53:58 +0200

    policy and average

--- ex08-pg/ex08-pg.py
+++ ex08-pg/ex08-pg.py
@@ -5,8 +5,15 @@ import matplotlib.pyplot as plt
 
 def policy(state, theta):
     """ TODO: return probabilities for actions under softmax action selection """
-    return [0.5, 0.5]  # both actions with 0.5 probability => random
+    weight_dim, num_action = theta.shape
+    policy = np.zeros(num_action)
+    e_h = [np.exp(action_preference(state, theta[:, i])) for i in range(num_action)]
+    for action in range(num_action):
+        policy[action] = e_h[action]/sum(e_h)
+    return policy
 
+def action_preference(state, parameters):
+    return np.dot(parameters, state)
 
 def generate_episode(env, theta, display=False):
     """ enerates one episode and returns the list of states, the list of rewards and the list of actions of that episode """
@@ -32,7 +39,7 @@ def generate_episode(env, theta, display=False):
 
 def REINFORCE(env):
     theta = np.random.rand(4, 2)  # policy parameters
-
+    last_ep_lens = []
     for e in range(10000):
         if e % 300 == 0:
             states, rewards, actions = generate_episode(env, theta, True)  # display the policy every 300 episodes
@@ -41,8 +48,14 @@ def REINFORCE(env):
 
         print("episode: " + str(e) + " length: " + str(len(states)))
         # TODO: keep track of previous 100 episode lengths and compute mean
+        # avg_len = avg_len + (len(states) - avg_len)/(e+1)
+        last_ep_lens.append(len(states))
+        if e >= 100:
+            last_ep_lens.pop(0)
+        avg_len = sum(last_ep_lens)/(e+1)
 
         # TODO: implement the reinforce algorithm to improve the policy weights
+
 
 
 
